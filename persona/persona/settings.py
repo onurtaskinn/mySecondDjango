0be6c6f7f832ea993978d08726409ffd33d5e4af
onurtaskinn <onurtaskin180@gmail.com>
2023-03-15 10:16:00 +0100

    allowed host changed

--- persona/persona/settings.py
+++ persona/persona/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g^_(%9j37*=1nhbbx!8o&5sq=e6isgw@%+izi6kz+mtv3c!ys2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myseconddjango.onrender.com']
 
 
 # Application definition
